35a3e959f1542c111a83432f81770981c4d3ba75
kassiocarvalhojk <82849257+kassiocarvalhojk@users.noreply.github.com>
2021-09-01 21:27:41 -0300

    Update DlistTest.java

--- DlistTest.java
+++ DlistTest.java
@@ -1,14 +1,35 @@
 package tests;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+import source.DList;
+import source.DNode;
 
 class DlistTest {
-
 	@Test
 	void test() {
-		fail("Not yet implemented");
-	}
+		DNode z, v;
+		DList l = new DList();
+		assertEquals("[]", l.toString(), "[]");
 
+		z = new DNode("BWI", null, null);
+		l.addLast(z);
+		assertEquals("[BWI]", l.toString(), "[BWI]");
+
+		z = new DNode("SFO", null, null);
+		l.addLast(z);
+		assertEquals("[BWI,SFO]", l.toString(), "[BWI,SFO]");
+
+		z = new DNode("JFK", null, null);
+		l.addFirst(z);
+		assertEquals("[JFK,BWI,SFO]", l.toString(), "[JFK,BWI,SFO]");
+
+		v = l.getLast(); // Pega o último node.
+		assertEquals("SFO", v.getElement(), "SFO");
+
+		z = new DNode("PVD", null, null);
+		l.addBefore(v, z);
+		assertEquals("[JFK,BWI,PVD,SFO]", l.toString(), "[JFK,BWI,PVD,SFO]");
+	}
 }
+
