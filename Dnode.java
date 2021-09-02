5333d55098652d41a5784213c907e097fa255158
kassiocarvalhojk <82849257+kassiocarvalhojk@users.noreply.github.com>
2021-09-01 21:27:15 -0300

    Update Dnode.java

--- Dnode.java
+++ Dnode.java
@@ -7,14 +7,26 @@ package source;
  * @author kassiocarvalho
  *
  */
-public class Dnode {
+//Nodo de uma lista duplamente encadeada de strings
+public class DNode {
+	protected String element; // String armazenada pelo nodo protected
+	DNode next, prev; // Ponteiros para o nodo seguinte e o anterior
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	// Construtor que cria um nodo com os campos fornecidos
+	public DNode(String e, DNode p, DNode n) { element = e; prev = p; next = n;}
 
-	}
+	// Retorna o elemento deste nodo
+	public String getElement() { return element; }
+	// Retorna o nodo anterior a este
+	public DNode getPrev() { return prev; }
+	// Retorna o nodo seguinte a este
+	public DNode getNext() { return next; }
 
+	// Atribui o elemento deste nodo
+	public void setElement(String newElem) { element = newElem;}
+	// Atribui o nodo anterior deste nodo
+	public void setPrev(DNode newPrev) { prev = newPrev; }
+	// Atribui o nodo seguinte a este nodo
+	public void setNext(DNode newNext) { next = newNext; }
 }
+
